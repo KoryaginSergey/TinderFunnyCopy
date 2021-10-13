b78dd977cbaeb71c3b6667f4d4841f67404b03e6
Koryagin Sergey <>
2021-10-13 14:45:31 +0300

    added empty project

---
+++ TinderFunnyCopy/TinderFunnyCopy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderFunnyCopy
+//
+//  Created by macuser on 13.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
